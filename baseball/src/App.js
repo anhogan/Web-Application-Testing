1e59855ba007a05cca0326bd45ac047e48b6cc00
Amanda Hogan <ahogan@teamworks.com>
2020-02-12 15:54:52 -0500

    Setup handler functions on class

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -1,17 +1,62 @@
 import React, { Component } from 'react';
+import Dashboard from './components/Dashboard';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      strike: 0,
+      ball: 0,
+      hit: 0
+    };
   };
-  
+
+  recordStrike() {
+    if(this.state.strike < 3) {
+      this.setState({
+        strike: strike + 1
+      });
+    } else {
+      this.setState({
+        strike: 0
+      });
+    };
+  };
+
+  recordBall() {
+    if(this.state.ball < 4) {
+      this.setState({
+        ball: ball + 1
+      });
+    } else {
+      this.setState({
+        ball: 0
+      });
+    };
+  };
+
+  recordFoul() {
+    if(this.state.strike < 3) {
+      this.setState({
+        strike: strike + 1
+      });
+    };
+  };
+
+  recordHit() {
+    this.setState({
+      strike: 0,
+      ball: 0,
+      hit: hit + 1
+    });
+  };
+
   render() {
     return (
       <div className="App">
-
+        <Dashboard />
       </div>
     );
   }
--- baseball/src/components/Dashboard.js
+++ baseball/src/components/Dashboard.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Display from './Display';
 
 const Dashboard = () => {
-
+  return (
+    <div>
+      <Display />
+    </div>
+  );
 };
 
 export default Dashboard;
